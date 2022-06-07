61f14c43411487840d5cbf3765ddcc97c8403c51
Muhammad Rafif <radif.adityo6@gmail.com>
2022-06-07 11:04:29 +0700

    Drawer Fragment

--- app/src/main/java/com/project/natheroes/fragment/HeroesGirlsFragment.kt
+++ app/src/main/java/com/project/natheroes/fragment/HeroesGirlsFragment.kt
@@ -5,38 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.project.natheroes.R
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.project.natheroes.adapter.HeroesGirlsAdapter
+import com.project.natheroes.databinding.FragmentHeroesGirlsBinding
 
 class HeroesGirlsFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentHeroesGirlsBinding? = null
+    private val binding get() = _binding!!
+
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_heroes_girls, container, false)
+        _binding = FragmentHeroesGirlsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        fun newInstance(param1: String, param2: String) =
-            HeroesGirlsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val heroesGirls = HeroesGirlsAdapter()
+       // heroesGirls.setDatas(newList = )
     }
+
 }
